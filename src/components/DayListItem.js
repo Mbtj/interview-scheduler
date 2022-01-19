eb8b301eaa97693381e892570beaaf418dbc56c3
TJ <michael.btj@hotmail.com>
2022-01-18 22:44:41 -0500

    add DayListItem formatting

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 export default function DayListItem(props) {
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return `no spots remaining`;
+    } if (props.spots === 1) {
+      return `1 spot remaining`;
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  };
   return (
     <li onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }

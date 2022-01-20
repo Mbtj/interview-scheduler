cf1334ed16a6c93fc5cd5325ceb2986c71fc3c58
TJ <michael.btj@hotmail.com>
2022-01-20 10:40:28 -0500

    add stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,44 @@
 import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
-  return (
+const { id, time, interview } = props
+
+return (
     <article className="appointment">
-      {
-        props.time &&
-        <Fragment>
-          Appointment at {props.time}
-        </Fragment>
-      }
-      {
-        !props.time &&
-        <Fragment>
-          No Appointments
-        </Fragment>
+      <Header time={time} />
+      
+      {interview
+      ? <Show
+        student={interview.student}
+        interviewer={interview.interviewer.name}
+        onEdit={() =>console.log("edit")}
+        onDelete={() => console.log("delete")}
+        />
+      : <Empty onAdd={() => console.log("add")}/>
       }
     </article>
   );
+
+  
+  // return (
+  //   <article className="appointment">
+  //     {
+  //       props.time &&
+  //       <Fragment>
+  //         Appointment at {props.time}
+  //       </Fragment>
+  //     }
+  //     {
+  //       !props.time &&
+  //       <Fragment>
+  //         No Appointments
+  //       </Fragment>
+  //     }
+  //   </article>
+  // );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -187,6 +187,22 @@ storiesOf("Appointment", module)
       student="Student Name"
       interviewer={1}
     />
-  );
+  )
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ));
 
   // REVISIT ONSAVE AND ONCANCEL 

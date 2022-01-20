ba475064f4e69392253087bbb70ec722ff3103c6
TJ <michael.btj@hotmail.com>
2022-01-19 20:17:01 -0500

    add text for story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      {
+        props.time &&
+        <Fragment>
+          Appointment at {props.time}
+        </Fragment>
+      }
+      {
+        !props.time &&
+        <Fragment>
+          No Appointments
+        </Fragment>
+      }
+    </article>
   );
 }

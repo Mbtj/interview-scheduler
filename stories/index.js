45e92944c37aa5bdae284cd1fb046aa737a1d4fa
TJ <michael.btj@hotmail.com>
2022-01-19 20:44:02 -0500

    add show component and story

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 const days = [
   {
@@ -144,4 +145,11 @@ storiesOf("InterviewerListItem", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => 
+  <Show 
+  student="Student Name"
+  interviewer="Interviewer Name"
+  onEdit={action("onEdit")}
+  onDelete={action("onDelete")}
+  />);

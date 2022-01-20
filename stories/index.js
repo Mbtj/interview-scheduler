34ff51f1520e3fc4c7d7ae79fa2a12f3391cb771
TJ <michael.btj@hotmail.com>
2022-01-19 21:04:58 -0500

    add Status component and story

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status"
 
 const days = [
   {
@@ -161,4 +162,5 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  );
+  )
+  .add("Status", () => <Status message="Deleting"/>);

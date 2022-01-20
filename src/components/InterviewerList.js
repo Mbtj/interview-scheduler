3a55da9c4aa163d4b9f6af273a8a114f78720b63
TJ <michael.btj@hotmail.com>
2022-01-19 19:59:45 -0500

    bug fix

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -23,8 +23,8 @@ export default function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === value}
-        setInterviewer={() => onChange(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });

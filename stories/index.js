86d2cf8f4c490a11c5a53b658ab5733280099389
TJ <michael.btj@hotmail.com>
2022-01-19 20:33:13 -0500

    add empty

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty";
 
 const days = [
   {
@@ -142,4 +143,5 @@ storiesOf("InterviewerListItem", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);

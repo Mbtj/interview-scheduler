62baf0ae504bcc0b6602bba4e304713e5c5fc2aa
TJ <michael.btj@hotmail.com>
2022-01-21 08:28:11 -0500

    add selector test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,12 @@
 function getAppointmentsForDay(state, day) {
   const filteredDay = state.days.filter(date => date.name === day);
-  return filteredDay.appointments;
+  if (filteredDay.length > 0) {
+    const appoinmentIDs = filteredDay[0].appointments;
+    console.log(appoinmentIDs);
+    return appoinmentIDs.map(id => state.appointments[id]);
+  } else {
+    return [];
+  }
 }
 
 module.exports = { getAppointmentsForDay }

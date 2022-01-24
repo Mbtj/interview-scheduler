4a4d00d418c4644f6b9177c670cc007d2b768a65
TJ <michael.btj@hotmail.com>
2022-01-24 15:47:46 -0500

    remove console log

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,7 +2,7 @@ function getAppointmentsForDay(state, day) {
   const filteredDay = state.days.filter(date => date.name === day);
   if (filteredDay.length > 0) {
     const appoinmentIDs = filteredDay[0].appointments;
-    console.log(appoinmentIDs);
+    // console.log(appoinmentIDs);
     return appoinmentIDs.map(id => state.appointments[id]);
   } else {
     return [];
